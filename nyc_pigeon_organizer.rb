1bd89f16c1fa380180ac05c0e4393ae5f3556ab7
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-28 13:58:53 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,19 +2,17 @@ def nyc_pigeon_organizer(data)
   pigeon_hash = {}
   
   all_names = []
-  unique_names = {}
+  unique_names = []
   
   data[:color].each do |key, value|
     data[:color][key].each do |value|
      all_names << value
     end
-    unique_names = all_names.uniq
+    unique_names.push(all_names.uniq)
   end
-  pigeon_hash = Hash[unique_names.collect{|element| [element, ""]}]
-
-  data.each do |category, value|
-    pigeon_hash[unique_names][category].push(value.to_s)
-  end
+ 
+ unique_names.each {|name| pigeon_hash[name] = {}}
+  
   
   return pigeon_hash
 end
